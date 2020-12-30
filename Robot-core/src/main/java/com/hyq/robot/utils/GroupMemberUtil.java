46647fc17c40459f4832473be170ff57189db0ac
nanke <1154685452@qq.com>
2020-12-30 18:29:58 +0800

    空白图片设置

--- Robot-core/src/main/java/com/hyq/robot/utils/GroupMemberUtil.java
+++ Robot-core/src/main/java/com/hyq/robot/utils/GroupMemberUtil.java
@@ -34,7 +34,8 @@ public class GroupMemberUtil {
                     .replace("图片" + memberDO.getLocation(),memberDO.getColor())
                     .replace("成员" + memberDO.getLocation(),memberDO.getMemberName());
         }
-        return originStr;
+        String htmlStr = replaceInit(teamName, originStr);
+        return htmlStr;
     }
 
     /**
@@ -48,59 +49,59 @@ public class GroupMemberUtil {
                 .replace("开团标题",teamName)
 
                 .replace("成员11","")
-                .replace("图片11","")
+                .replace("图片11","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员12","")
-                .replace("图片12","")
+                .replace("图片12","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员13","")
-                .replace("图片13","")
+                .replace("图片13","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员14","")
-                .replace("图片14","")
+                .replace("图片14","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员15","")
-                .replace("图片15","")
+                .replace("图片15","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
 
                 .replace("成员21","")
-                .replace("图片21","")
+                .replace("图片21","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员22","")
-                .replace("图片22","")
+                .replace("图片22","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员23","")
-                .replace("图片23","")
+                .replace("图片23","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员24","")
-                .replace("图片24","")
+                .replace("图片24","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员25","")
-                .replace("图片25","")
+                .replace("图片25","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
 
                 .replace("成员31","")
-                .replace("图片31","")
+                .replace("图片31","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员32","")
-                .replace("图片32","")
+                .replace("图片32","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员33","")
-                .replace("图片33","")
+                .replace("图片33","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员34","")
-                .replace("图片34","")
+                .replace("图片34","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员35","")
-                .replace("图片35","")
+                .replace("图片35","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
 
                 .replace("成员41","")
-                .replace("图片41","")
+                .replace("图片41","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员42","")
-                .replace("图片42","")
+                .replace("图片42","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员43","")
-                .replace("图片43","")
+                .replace("图片43","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员44","")
-                .replace("图片44","")
+                .replace("图片44","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员45","")
-                .replace("图片45","")
+                .replace("图片45","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
 
                 .replace("成员51","")
-                .replace("图片51","")
+                .replace("图片51","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员52","")
-                .replace("图片52","")
+                .replace("图片52","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员53","")
-                .replace("图片53","")
+                .replace("图片53","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员54","")
-                .replace("图片54","")
+                .replace("图片54","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png")
                 .replace("成员55","")
-                .replace("图片55","");
+                .replace("图片55","https://oss.jx3box.com/upload/post/2020/12/30/1901382.png");
     }
 
     /**
